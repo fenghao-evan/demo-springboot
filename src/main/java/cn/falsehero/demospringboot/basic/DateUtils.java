dbf0cf854226d866103f1a81a5cf6d3cc620eb7d
evafeng <fh@falsehero.cn>
2021-01-05 20:49:32 +0800

    add DateUtils.java

---
+++ src/main/java/cn/falsehero/demospringboot/basic/DateUtils.java
@@ -0,0 +1,14 @@
+package cn.falsehero.demospringboot.basic;
+
+/**
+ * @Author: fenghao
+ * @Date: 2021/01/04 22:44
+ * @Description:
+ */
+public class DateUtils {
+
+    public static long nowTimestamp() {
+        return System.currentTimeMillis();
+    }
+
+}
