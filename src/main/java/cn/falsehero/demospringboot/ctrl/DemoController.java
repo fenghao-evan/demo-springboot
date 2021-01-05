738aef22976b75981051fe95a9daf24ada549123
evafeng <fh@falsehero.cn>
2021-01-05 22:10:23 +0800

    添加统一接口前缀

--- src/main/java/cn/falsehero/demospringboot/ctrl/DemoController.java
+++ src/main/java/cn/falsehero/demospringboot/ctrl/DemoController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description:
  */
 @RestController
-@RequestMapping("/api/demo")
+@RequestMapping("/demo")
 public class DemoController {
 
     @GetMapping("/getDemo")
