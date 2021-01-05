d117eec19ab0a100ad3385a4a7cb9fe0e81b55d3
evafeng <fh@falsehero.cn>
2021-01-05 20:50:19 +0800

    Demo 修改

--- src/main/java/cn/falsehero/demospringboot/ctrl/DemoController.java
+++ src/main/java/cn/falsehero/demospringboot/ctrl/DemoController.java
@@ -1,6 +1,9 @@
 package cn.falsehero.demospringboot.ctrl;
 
+import cn.falsehero.demospringboot.basic.Result;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,11 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date: 2020/12/06 13:46
  * @Description:
  */
-@RestController("/demo")
+@RestController
+@RequestMapping("/demo")
 public class DemoController {
 
     @GetMapping("/getDemo")
-    public String getDemo() {
-        return "this is a demo";
+    public Result<String> getDemo() {
+        Result<String> result = Result.newInstance();
+        result.setObject("this is demo;;;");
+        return result;
+    }
+
+    @PostMapping("/postDemo")
+    public String postDemo() {
+        return "this is post";
     }
 }
